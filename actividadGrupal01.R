71a6fb81a1d4499319c072bd32ee91bab63e6717
Ignacio Gamboa <ign.gga@outlook.cl>
2024-03-23 13:02:50 -0300

    Se llama al archivo que se encuentra en directorio. Se obtienen primeros datos.

--- actividadGrupal01.R
+++ actividadGrupal01.R
@@ -1,2 +1,8 @@
 rm(list=ls()) #Limpiar consola
 setwd("C:/Users/igngg/OneDrive - UNIVERSIDAD ANDRES BELLO/Documents/003_tercerAño_2024/05_mineriaDeDatos/Grupo_9/Directorio") #Nuevo directorio
+
+conjuntoDeDatos<-read.csv('Directorio/precio_diadm.csv')
+
+dim(conjuntoDeDatos)
+head(conjuntoDeDatos)
+str(conjuntoDeDatos)
