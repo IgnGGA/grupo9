b5954e689f288b6caf51c29a788db8c6286c8700
Ignacio Gamboa <ign.gga@outlook.cl>
2024-03-24 14:58:18 -0300

    Se realiza un par de codigos y generación de algunas tablas para despues realizar graficos de barra.

--- actividadGrupal01.R
+++ actividadGrupal01.R
@@ -1,13 +1,37 @@
 rm(list=ls()) #Limpiar consola
 
 library(qcc)
-library(funModeling)
+library(ggplot2)
 
 setwd("C:/Users/igngg/OneDrive - UNIVERSIDAD ANDRES BELLO/Documents/003_tercerAño_2024/05_mineriaDeDatos/Grupo_9") #Nuevo directorio
 conjuntoDeDatos<-read.csv('Directorio/precio_diadm.csv')
+
+names(conjuntoDeDatos)<-c('Peso','Corte','Color','Claridad','Profundidad','Ancho','Precio','X','Y','Z')
 
 dim(conjuntoDeDatos)
 str(conjuntoDeDatos)
 head(conjuntoDeDatos)
 
-hist(conjuntoDeDatos$carat)
+hist(conjuntoDeDatos$Peso,
+     main='Histograma de Peso',
+     xlab='Peso',
+     ylab='Frecuencia')
+
+tb.freq<-function(x){
+  f_i<-as.vector(table(x))
+  tf<-cbind(f_i)
+  row.names(tf)<-names(table(x))
+  return(tf)
+}
+
+tabPeso<-tb.freq(conjuntoDeDatos$Peso)
+tabPeso
+tabPrecio<-tb.freq(conjuntoDeDatos$Precio)
+tabPrecio
+tabCorte<-tb.freq(conjuntoDeDatos$Corte)
+tabCorte
+graphPeso<-barplot(tabPrecio,
+                   main='Peso de diamantes',
+                   xlab='Peso',
+                   ylab='Frecuencia',
+                   border='black')
