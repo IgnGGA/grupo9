4e82eee901ebf377e21cee4ab1f0b8b9fbf74f9f
Ignacio Gamboa <ign.gga@outlook.cl>
2024-03-28 23:05:06 -0300

    Se genera funcion del profe para establecer el coeficiente de asimetria. Tambien se obtienen estadisticos de la columna precio

--- actividadGrupal02_20240328.R
+++ actividadGrupal02_20240328.R
@@ -2,6 +2,12 @@ rm(list=ls()) #Limpiar consola
 
 library(ggplot2)
 
+c.coefAsim<-function(x){
+  a=mean((x-mean(x,na.rm=T))^3,na.rm=T)
+  coefAsim=a/(sd(x,na.rm = T)^3)
+  coefAsim
+}
+
 setwd("C:/Users/igngg/OneDrive - UNIVERSIDAD ANDRES BELLO/Documents/003_tercerAño_2024/05_mineriaDeDatos/Grupo_9") #Nuevo directorio
 
 diamantes<-read.csv('Directorio/precio_diadm.csv', sep=',', header = T)
@@ -10,11 +16,23 @@ diamantes
 str(diamantes)#Observamos los tipos de variables por columnas
 summary(diamantes)#obtenemos un sumario respecto al estado de las columas establecidas
 
-tablaCorte<-table(diamantes$Corte)
-tablaCorte#Podemos obtener la cantidad de cada uno de los cortes.
+tablaCorte<-table(diamantes$Corte);tablaCorte#Podemos obtener la cantidad de cada uno de los cortes.
 
-tablaColor<-table(diamantes$Color)
-tablaColor
+tablaColor<-table(diamantes$Color);tablaColor
 
-tablaClaridad<-table(diamantes$Claridad)
-tablaClaridad
+tablaClaridad<-table(diamantes$Claridad);tablaClaridad
+
+#Seria importante econtrar una relación entre el precio del diamantes y alguna de sus caracteristicas.
+#En primera instancia obtener los estadisticos de la comuna 'Precio' de los diamantes.
+
+#Estadisticos 'Precio' (promPrc, mediaPrc, etc...)
+Prc<-diamantes$Precio
+promPrc<-mean(Prc);promPrc
+mediaPrc<-median(Prc);mediaPrc#Mediana
+Q1Prc<-quantile(Prc, probs = 0.25)[[1]];Q1Prc#Primer quintil
+Q3Prc<-quantile(Prc, probs=0.75)[[1]];Q3Prc#Tercer Quintil
+varPrc<-var(Prc);varPrc#Varianza
+sdPrc<-sd(Prc);sdPrc#Desviacion estarndar
+IQRPrc<-IQR(Prc);IQRPrc#Rango inter quintil
+hist(Prc)#Histograma del precio, se observa que tiene un comportamiento atipico :s
+
