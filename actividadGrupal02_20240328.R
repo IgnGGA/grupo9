a7f8142b4e2e323a514efdb2885a06a9e40a00a3
Ignacio Gamboa <ign.gga@outlook.cl>
2024-03-28 22:31:04 -0300

    Se genera el archivo con la actividad grupal 2 on fecha del 28/03/2024 y segun las indicaciones de la clase 4

---
+++ actividadGrupal02_20240328.R
@@ -0,0 +1,11 @@
+rm(list=ls()) #Limpiar consola
+
+library(ggplot2)
+
+setwd("C:/Users/igngg/OneDrive - UNIVERSIDAD ANDRES BELLO/Documents/003_tercerAño_2024/05_mineriaDeDatos/Grupo_9") #Nuevo directorio
+
+diamantes<-read.csv('Directorio/precio_diadm.csv', sep=',', header = T)
+names(diamantes)<-c('Peso','Corte','Color','Claridad','Profundidad','Ancho','Precio','X','Y','Z')
+diamantes
+str(diamantes)#Observamos los tipos de variables por columnas
+summary(diamantes)#obtenemos un sumario respecto al estado de las columas establecidas
