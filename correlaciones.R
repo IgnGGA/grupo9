ed4b6901238bb2ed600e3bf48a4efe1fc6ae469b
Ignacio Gamboa <ign.gga@outlook.cl>
2024-03-30 08:51:43 -0300

    Se inicia in script para emprezar a buscar correlaciones entre las variables de interes. en este caso solo se esta evaluando el peso y el precio. aun que faltan aun ver otras caracteristicas de los diamantes.

---
+++ correlaciones.R
@@ -0,0 +1,21 @@
+ggplot(diamantes,aes(x=Precio,y=Peso))+
+  geom_point()+
+  geom_smooth(method=lm, se=FALSE)+
+  labs(x = "Precio", y = "Peso")+
+  theme(text = element_text(size=14))+
+  theme_grey(base_size = 16)
+#Segun yo, la correolacion existente no tiene un comportamiento lineal, aun que si es ascendente...
+
+##Coeficientes de correlación
+#covarianza
+cov(Pso,Prc)#La covarianza es positiva, por lo tanto hay una dependencia directa 
+#correlación de Pearson
+cor(Pso,Prc)#La correlacion es cercano a 1, por lo tanto, si hay una correlacion entre las variables 
+#Ho:cor=0 H1:cor<>0
+cor.test(Pso,Prc)
+#correlación de Spearman automático
+cor(Pso,Prc,method = "pearson")
+
+cor.test(Pso,Prc,method = "pearson")
+##Tau de kendall
+cor(Pso,Prc,method = "kendall")#podriamos decir que existe una tendencia a la correlacion entre ambras variables.
