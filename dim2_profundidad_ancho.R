1e1e58135478cdb0f78f910918fd684286969f32
Ignacio Gamboa <ign.gga@outlook.cl>
2024-04-11 20:53:50 -0400

    Se realiza obtencion de varianza y desviación estandar, y graficas, aun que no me convence si tiene una tendencia a una distribuicion normal

--- dim2_profundidad_ancho.R
+++ dim2_profundidad_ancho.R
@@ -6,3 +6,15 @@ medProf=median(Prof);medProf
 medAncho=median(Ancho);medAncho
 #El promedio y la media de ambas variables se encuentran cercanas
 #Esto nos da indicios que podria tener un comportamiento de distribuicion normal
+varProf=var(Prof);varProf
+varAncho=var(Ancho);varAncho
+sdProf=sd(Prof);sdProf
+sdAncho=sd(Ancho);sdAncho
+hist(Prof,
+     main='Histograma de Profundidad',
+     xlab='Profundidad',
+     ylab='Frecuencia')
+hist(Ancho,
+     main='Histograma de Ancho',
+     xlab='Ancho',
+     ylab='Frecuencia')
