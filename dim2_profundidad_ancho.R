3a4bc6bff7a1ab55d05e5cbe3441a1493f78fae6
Ignacio Gamboa <ign.gga@outlook.cl>
2024-04-11 20:47:57 -0400

    Se comiensa analisis de Dim2 respecto a las variables profundidad y ancho

---
+++ dim2_profundidad_ancho.R
@@ -0,0 +1,8 @@
+Prof=diamantes$Profundidad
+Ancho=diamantes$Ancho
+meanProf=mean(Prof);meanProf
+meanAncho=mean(Ancho);meanAncho
+medProf=median(Prof);medProf
+medAncho=median(Ancho);medAncho
+#El promedio y la media de ambas variables se encuentran cercanas
+#Esto nos da indicios que podria tener un comportamiento de distribuicion normal
