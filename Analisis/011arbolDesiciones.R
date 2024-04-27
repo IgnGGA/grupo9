3a0380d3bcc66a7cd1ccf40b2a6dbdee4593b6ef
Ignacio Gamboa <ign.gga@outlook.cl>
2024-04-27 18:01:52 -0400

    Se elimina redundancia de la libreria. Se actualiza como la ariable de interes el precio y no la prundidad.

--- Analisis/011arbolDesiciones.R
+++ Analisis/011arbolDesiciones.R
@@ -6,8 +6,8 @@ selectrows <- sample(1:nrow(ejemData),round(0.80*nrow(ejemData)))
 selectrows
 dat.train <- ejemData[selectrows,]
 dat.test <- ejemData[-selectrows,]
-library(rpart)
-tree <- rpart(Profundidad ~., data = dat.train, 
+
+tree <- rpart(Precio ~., data = dat.train, 
               method = "anova",cp=0.001)
 rpart.plot(tree,fallen.leaves = FALSE, cex=0.65)
 table(pred=predict(tree, type = "class"),obs=train.data$diabetes)
