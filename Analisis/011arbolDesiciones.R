fdee4818fed5c62b73ab98bc1e98025653c91a1a
Ignacio Gamboa <ign.gga@outlook.cl>
2024-04-27 22:57:16 -0400

    Se inicia proceso de predicciones.

--- Analisis/011arbolDesiciones.R
+++ Analisis/011arbolDesiciones.R
@@ -10,4 +10,24 @@ dat.test <- ejemData[-selectrows,]
 tree <- rpart(Precio ~., data = dat.train, 
               method = "anova",cp=0.001)
 rpart.plot(tree,fallen.leaves = FALSE, cex=0.65)
-table(pred=predict(tree, type = "class"),obs=train.data$diabetes)
+bestcp2 <- tree$cptable[which.min(tree$cptable[,"xerror"]),"CP"]
+bestcp2  
+
+ejemData2 <- prune(tree, cp = bestcp2)
+rpart.plot(ejemData2, box.palette="GnBu",
+           branch.lty=3, shadow.col="gray", nn=TRUE)
+
+ejemData2$variable.importance %>% 
+  data.frame() %>%
+  rownames_to_column(var = "Feature") %>%
+  rename(Overall = '.') %>%
+  ggplot(aes(x = fct_reorder(Feature, Overall), y = Overall)) +
+  geom_pointrange(aes(ymin = 0, ymax = Overall), color = "cadetblue", size = .3) +
+  theme_minimal() +
+  coord_flip() +
+  labs(x = "", y = "", title = "Importancia")
+  
+dat.test$pred1<-predict(ejemData,dat.test)
+dat.train$pred1<-predict(ejemData,dat.train)
+val.ejemData<-med.reg(dat.test$Precio,dat.test$pred1)
+val.ejemData_2<-med.reg(dat.train$Precio,dat.train$pred1)
