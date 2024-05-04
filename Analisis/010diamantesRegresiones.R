38eabdf3f9287d0b8b0d568253e1f5c163ef0a02
Ignacio Gamboa <ign.gga@outlook.cl>
2024-05-03 23:52:13 -0400

    avanzando en regresion lineal <3

--- Analisis/010diamantesRegresiones.R
+++ Analisis/010diamantesRegresiones.R
@@ -3,7 +3,14 @@ diamond<-diamantes[,-c(2,3,4)]# al tadaframe le extraemos las columnas con datos
 
 pca_diamond <- prcomp(diamond,center=TRUE,scale = TRUE)
 pca_diamond
-summary(pca_diamond)
 
 res.var <- get_pca_var(pca_diamond)
 corrplot(res.var$cos2, is.corr=FALSE)
+pca_diamond$center
+pca_diamond$scale
+pca_diamond$rotation
+
+summary(pca_diamond)
+biplot(pca_diamond)
+
+pca_diamond$x
