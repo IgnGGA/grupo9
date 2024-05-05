75d79abf705dbf65068ac4e944c742895361d086
Ignacio Gamboa <ign.gga@outlook.cl>
2024-05-05 12:23:06 -0400

    a ejecutaar to_dummy con las tres variables cuatitativas

--- Analisis/0012redesNeuronales.R
+++ Analisis/0012redesNeuronales.R
@@ -1,38 +1,17 @@
+To_Dummy(diamantes$Talla)
+To_Dummy(diamantes$Color)
+To_Dummy(diamantes$Pureza)
+
 set.seed(123)
 training.samples <- diamantes$Precio %>% 
   createDataPartition(p = 0.8, list = FALSE)
 dat.train  <- diamantes[training.samples, ]
 dat.test <- diamantes[-training.samples, ]
 
-#regresion lineal muntiple
-modo1<-lm(Precio~.,data =  dat.train)
+maxs <- apply(diamantes, 2, max) 
+mins <- apply(diamantes, 2, min)
+scaled <- as.data.frame(scale(diamantes, center = mins, scale = maxs - mins))
+dtrain.scal <- scaled[selectrows,]
+dtest.scal<- scaled[-selectrows,]
 
-str(dat.train)
-dat.train_aux<-dat.train[,-c(2,3,4)]
-str(dat.train[,c(2,3,4)])
-table(dat.train$Talla)
-table(dat.train$Color)
-table(dat.train$Pureza)
-
-
-dat.train$Talla<-as.character(dat.train$Talla)
-
-
-names<-unique(dat.train$Talla)
-Ieal_DM<-ifelse(dat.train$Talla==names[1],1,0)
-Prim_DM<-ifelse(dat.train$Talla==names[2],1,0)
-
-dat.train$Color<-as.factor(dat.train$Color)
-dat.train$Pureza<-as.factor(dat.train$Pureza)
-str(dat.train)
-
-function DummyT(variable){
-  names<-unique(variable)
-  for(i in names){
-    assign(paste0(i,"_DM"),ifelse(variable==i,1,0))
-  }
-  return(df)
-
-}
-
-regnn1 <- neuralnet(Precio ~.,data=dat.train,hidden=c(5,3),linear.output=T)
+regnn1 <- neuralnet(Precio ~.,data=dat.train,hidden=c(7),linear.output=T)
