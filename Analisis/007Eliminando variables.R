2f97881d1b837009c03be9109aca59acda6a8af4
Ignacio Gamboa <ign.gga@outlook.cl>
2024-05-25 20:37:53 -0400

    Para eliminadoir de variables, solo de deja eliminar los datos que tengan solo dimensiones irreales )que alguna de sus dimenciones sea igual a cero)

--- Analisis/007Eliminando variables.R
+++ Analisis/007Eliminando variables.R
@@ -1,15 +1,5 @@
 #Eliminador de datos atipicos
-
-Q1<-quantile(diamantes$Precio, probs = 0,25)[[1]]
-Q3<-quantile(diamantes$Precio, probs = 0,75)[[1]]
-RIV=1.5*IQR(diamantes$Precio)
-for (i in length(diamantes$Precio)){
-  diamantes<-diamantes[!(diamantes$Precio>Q3+RIV),]
-  diamantes<-diamantes[!(diamantes$Precio<Q1-RIV),]}
-boxplot(diamantes$Z)
-
-
-diamantes<-diamantes[!(diamantes$X==0.0),]
-diamantes<-diamantes[!(diamantes$Y==0.0),]
-diamantes<-diamantes[!(diamantes$Z==0.0),]
+diamantes<-diamantes[!(diamantes$x==0.0),]
+diamantes<-diamantes[!(diamantes$y==0.0),]
+diamantes<-diamantes[!(diamantes$z==0.0),]
 summary(diamantes)
