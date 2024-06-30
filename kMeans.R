24651183ed7e74e8dc0d08c5157b4f1914faf780
Ignacio Gamboa <ign.gga@outlook.cl>
2024-06-29 22:01:19 -0400

    se trabajara con el total del set de datos

--- kMeans.R
+++ kMeans.R
@@ -14,19 +14,13 @@ diamantes$cut[diamantes$cut%in%"Premium"]<-"4"
 diamantes$cut[diamantes$cut%in%"Ideal"]<-"5"
 diamantes$cut<-as.numeric(diamantes$cut)
 
-str(diamantes)
-set.seed(123)
-selectrows <- sample(1:nrow(diamantes),round(0.75*nrow(diamantes)))
-muestra <- diamantes[-selectrows,]
-elResto <- diamantes[selectrows,]
-
 #sacar precio del conjunto de datos
 #pasar a dummy caracteristicas
 #Realizar PCA
 #aplicar ks
 #establecer rangos de precio para comparacion
 #en caso de no generar buenos resultados, sin considerar vr. dummy
-pcaD<-prcomp(muestra,center=TRUE,scale=TRUE)
+pcaD<-prcomp(diamantes,center=TRUE,scale=TRUE)
 summary(pcaD)
 res.var <- get_pca_var(pcaD)
 corrplot(res.var$cos2, is.corr=FALSE)
@@ -49,26 +43,26 @@ fviz_nbclust(pcaD_ux, kmeans, method = "gap_stat")
 fit2<-kmeans(pcaD_ux, 2)
 table(fit2$cluster)
 barplot(table(fit2$cluster),col=c(2,3))
-fviz_cluster(fit2, data = muestra)
+fviz_cluster(fit2, data = diamantes)
 #funModeling::freq(fit1$clus)
 #evaluar modelo
-muestra_aux <- diamantes_aux[-selectrows,]
-muestra_aux$clus<-as.vector(fit2$cluster)
-head(muestra_aux)
+diamantes_aux <- diamantes_aux[-selectrows,]
+diamantes_aux$clus<-as.vector(fit2$cluster)
+head(diamantes_aux)
 
-ggplot(muestra_aux, aes(y=price, x=factor(clus))) + 
+ggplot(diamantes_aux, aes(y=price, x=factor(clus))) + 
   geom_boxplot(fill="lightgreen",varwidth = T)+
   labs(x = "clus", y = "price")+
   theme(text = element_text(size=14))+
   theme_grey(base_size = 16)
 
-ggplot(muestra_aux, aes(y=depth, x=factor(clus))) + 
+ggplot(diamantes_aux, aes(y=depth, x=factor(clus))) + 
   geom_boxplot(fill="lightgreen",varwidth = T)+
   labs(x = "clus", y = "depth")+
   theme(text = element_text(size=14))+
   theme_grey(base_size = 16)
 
-ggplot(muestra_aux, aes(y=table, x=factor(clus))) + 
+ggplot(diamantes_aux, aes(y=table, x=factor(clus))) + 
   geom_boxplot(fill="lightgreen",varwidth = T)+
   labs(x = "clus", y = "table")+
   theme(text = element_text(size=14))+
